9d13c16b0a2485fe35c4c1bed65a45a99e28bf2c
Ziwei_Data_Science <132317686+xavierStorage@users.noreply.github.com>
2023-05-01 20:41:40 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,23 @@
 import streamlit as st
+import pandas as pd
 
-st.header('翻译')
+# Set the title of the web app
+st.title("CSV File Uploader")
 
-if st.button('点击翻译'):
-     st.write('You are a pig, you only know eat eat eat')
-else:
-     st.write('Eres cerdo, solo sabes comer comer comer')
+# Create a file uploader and prompt the user to upload a CSV file
+csv_file = st.file_uploader("Upload a CSV file", type=["csv"])
+
+# If the user has uploaded a CSV file
+if csv_file is not None:
+    # Load the CSV file into a Pandas dataframe
+    df = pd.read_csv(csv_file)
+    # Display the dataframe in the Streamlit app
+    st.write(df)
+    
+    # Add a button to download the CSV file
+    st.download_button(
+        label="Download CSV",
+        data=df.to_csv().encode("utf-8"),
+        file_name="my_csv_file.csv",
+        mime="text/csv"
+    )
