0d8490654aadea231a4f3724aad780653e84e508
eres-cerdo <132317686+eres-cerdo@users.noreply.github.com>
2023-05-01 19:09:12 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
-st.header('st.button')
+st.header('子为的翻译软件')
 
-if st.button('Say hello'):
-     st.write('Why hello there')
+if st.button('点击翻译'):
+     st.write('You are a pig, you only know eat eat eat')
 else:
-     st.write('Goodbye')
+     st.write('Eres cerdo, solo sabes comer comer comer')
