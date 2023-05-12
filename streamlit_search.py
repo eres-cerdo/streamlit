2894d67f64110a9e63da1ba11bdd83508c417414
Ziwei_Data_Science <132317686+xavierStorage@users.noreply.github.com>
2023-05-11 21:31:04 -0700

    Update streamlit_search.py

--- streamlit_search.py
+++ streamlit_search.py
@@ -6,14 +6,24 @@ def search_on_google(query):
     search_url = f"https://www.google.com/search?q={query}"
     return search_url
 
+def search_on_taobao(query):
+    search_url = f"https://www.taobao.com/search?q={query}"
+    return search_url
+
 # Streamlit app
 def main():
-    st.title("Google Search App")
+    st.title("Search App")
     query = st.text_input("Enter your search term:")
     if st.button("Search"):
         if query:
-            search_url = search_on_google(query)
-            html_string = f'<a href="{search_url}" target="_blank">Open Search Results</a>'
+            google_url = search_on_google(query)
+            taobao_url = search_on_taobao(query)
+            html_string = f'''
+                <h2>Google Search:</h2>
+                <a href="{google_url}" target="_blank">Open Google Search Results</a>
+                <h2>Taobao Search:</h2>
+                <a href="{taobao_url}" target="_blank">Open Taobao Search Results</a>
+            '''
             html(html_string, scrolling=True)
         else:
             st.warning("Please enter a search term.")
