cc119b0dcfdfb7c7aba28c0cd51ac1a98e5b98b2
Ziwei_Data_Science <132317686+xavierStorage@users.noreply.github.com>
2023-05-11 22:11:55 -0700

    Update streamlit_search.py

--- streamlit_search.py
+++ streamlit_search.py
@@ -10,6 +10,10 @@ def search_on_taobao(query):
     search_url = f"https://s.taobao.com/search?catId=100&from=sea_1_searchbutton&page=1&q={query}&spm=a2141.241046-cn.searchbar.d_2_searchbox&tmhkh5=&type=p"
     return search_url
 
+def search_on_baidu(query):
+    search_url = f"https://www.baidu.com/s?wd={query}"
+    return search_url
+
 # Streamlit app
 def main():
     st.title("Search App")
@@ -18,13 +22,19 @@ def main():
         if query:
             google_url = search_on_google(query)
             taobao_url = search_on_taobao(query)
+            baidu_url = search_on_baidu(query)
             html_string = f'''
                 <h2 style="font-size: 24px; color: green;">Google Search:</h2>
                 <a href="{google_url}" target="_blank" style="font-size: 18px;">Open Google Search Results</a>
                 <h2 style="font-size: 24px; color: green;">Taobao Search:</h2>
                 <a href="{taobao_url}" target="_blank" style="font-size: 18px;">Open Taobao Search Results</a>
+                <h2 style="font-size: 24px; color: green;">Baidu Search:</h2>
+                <a href="{baidu_url}" target="_blank" style="font-size: 18px;">Open Baidu Search Results</a>
             '''
-            html(html_string,height=600)
+            html(html_string, height=600)
+            st.write(f"Google Search URL: {google_url}")
+            st.write(f"Taobao Search URL: {taobao_url}")
+            st.write(f"Baidu Search URL: {baidu_url}")
         else:
             st.warning("Please enter a search term.")
 
