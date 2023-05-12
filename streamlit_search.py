69dd64bc409e95942a6e478b0dcbfc0cef04b57c
Ziwei_Data_Science <132317686+xavierStorage@users.noreply.github.com>
2023-05-11 21:26:06 -0700

    Update streamlit_search.py

--- streamlit_search.py
+++ streamlit_search.py
@@ -1,9 +1,10 @@
 import streamlit as st
+from streamlit.components.v1 import html
 import webbrowser
 
 def search_on_google(query):
     search_url = f"https://www.google.com/search?q={query}"
-    webbrowser.open_new_tab(search_url)
+    return search_url
 
 # Streamlit app
 def main():
@@ -11,8 +12,9 @@ def main():
     query = st.text_input("Enter your search term:")
     if st.button("Search"):
         if query:
-            search_on_google(query)
-            st.success(f"Searching for '{query}'...")
+            search_url = search_on_google(query)
+            html_string = f'<a href="{search_url}" target="_blank">Open Search Results</a>'
+            html(html_string, scrolling=True)
         else:
             st.warning("Please enter a search term.")
 
