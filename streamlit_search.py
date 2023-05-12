45da99c5d953e895958fbb54962b75d4787b6af5
Ziwei_Data_Science <132317686+xavierStorage@users.noreply.github.com>
2023-05-11 22:42:11 -0700

    Update streamlit_search.py

--- streamlit_search.py
+++ streamlit_search.py
@@ -1,52 +1,15 @@
 import streamlit as st
-from streamlit.components.v1 import html
 import webbrowser
 
-def search_on_google(query):
-    search_url = f"https://www.google.com/search?q={query}"
-    return search_url
+def open_urls(urls):
+    for url in urls:
+        webbrowser.open_new_tab(url)
 
-def search_on_taobao(query):
-    search_url = f"https://s.taobao.com/search?catId=100&from=sea_1_searchbutton&page=1&q={query}&spm=a2141.241046-cn.searchbar.d_2_searchbox&tmhkh5=&type=p"
-    return search_url
+# Streamlit app code
+st.title("Open Websites")
+st.write("Click the button below to open Google.com and Taobao.com.")
 
-def search_on_baidu(query):
-    search_url = f"https://www.baidu.com/s?wd={query}"
-    return search_url
-
-# Open all search result links
-def open_all_links():
-    query = st.session_state.search_query
-    if query:
-        google_url = search_on_google(query)
-        taobao_url = search_on_taobao(query)
-        baidu_url = search_on_baidu(query)
-        webbrowser.open(google_url, new=2)
-        webbrowser.open(taobao_url, new=2)
-        webbrowser.open(baidu_url, new=2)
-
-# Streamlit app
-def main():
-    st.title("Search App")
-    query = st.text_input("Enter your search term:", key="search_query")
-    if st.button("Search"):
-        if query:
-            google_url = search_on_google(query)
-            taobao_url = search_on_taobao(query)
-            baidu_url = search_on_baidu(query)
-            html_string = f'''
-                <h2 style="font-size: 24px; color: green;">Google Search:</h2>
-                <a href="{google_url}" target="_blank" style="font-size: 18px;">Open Google Search Results</a>
-                <h2 style="font-size: 24px; color: green;">Taobao Search:</h2>
-                <a href="{taobao_url}" target="_blank" style="font-size: 18px;">Open Taobao Search Results</a>
-                <h2 style="font-size: 24px; color: green;">Baidu Search:</h2>
-                <a href="{baidu_url}" target="_blank" style="font-size: 18px;">Open Baidu Search Results</a>
-            '''
-            html(html_string, height=1600)
-            st.write(f"Google Search URL: {google_url}")
-            st.write(f"Taobao Search URL: {taobao_url}")
-            st.write(f"Baidu Search URL: {baidu_url}")
-            st.button("Open All Links", on_click=open_all_links)
-
-if __name__ == "__main__":
-    main()
+# Create a button to open Google.com and Taobao.com
+if st.button("Open Google and Taobao"):
+    urls = ["https://www.google.com", "https://www.taobao.com"]
+    open_urls(urls)
